e42e966260e310e0382fa217e86c12b896ed910f
Matthew Julian <matthewjulian.bdg@gmail.com>
2025-03-17 10:27:03 +0700

    Test zoom iOS

--- lib/qr_view.dart
+++ lib/qr_view.dart
@@ -130,55 +130,55 @@ class _QRViewExampleState extends State<QRViewExample> {
     );
   }
 
+  // Widget _buildQrView(BuildContext context) {
+  //   // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+  //   var scanArea =
+  //       (MediaQuery.of(context).size.width < 400 ||
+  //               MediaQuery.of(context).size.height < 400)
+  //           ? 150.0
+  //           : 300.0;
+  //   // To ensure the Scanner view is properly sizes after rotation
+  //   // we need to listen for Flutter SizeChanged notification and update controller
+  //   return QRView(
+  //     key: qrKey,
+  //     onQRViewCreated: _onQRViewCreated,
+  //     overlay: QrScannerOverlayShape(
+  //       borderColor: Colors.red,
+  //       borderRadius: 10,
+  //       borderLength: 30,
+  //       borderWidth: 10,
+  //       cutOutSize: scanArea,
+  //     ),
+  //     onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+  //   );
+  // }
+
   Widget _buildQrView(BuildContext context) {
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
     var scanArea =
         (MediaQuery.of(context).size.width < 400 ||
                 MediaQuery.of(context).size.height < 400)
             ? 150.0
             : 300.0;
-    // To ensure the Scanner view is properly sizes after rotation
-    // we need to listen for Flutter SizeChanged notification and update controller
-    return QRView(
-      key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
-      overlay: QrScannerOverlayShape(
-        borderColor: Colors.red,
-        borderRadius: 10,
-        borderLength: 30,
-        borderWidth: 10,
-        cutOutSize: scanArea,
+
+    // Tambahkan Transform.scale di sini
+    return Center(
+      child: Transform.scale(
+        scale: Platform.isIOS ? 2.0 : 1.0, // Zoom 2x hanya di iOS
+        child: QRView(
+          key: qrKey,
+          onQRViewCreated: _onQRViewCreated,
+          overlay: QrScannerOverlayShape(
+            borderColor: Colors.red,
+            borderRadius: 10,
+            borderLength: 30,
+            borderWidth: 10,
+            cutOutSize: scanArea,
+          ),
+          onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+        ),
       ),
-      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
     );
   }
-
-  // void _onQRViewCreated(QRViewController controller) {
-  //   setState(() {
-  //     this.controller = controller;
-  //   });
-  //   controller.scannedDataStream.listen((scanData) {
-  //     setState(() {
-  //       result = scanData;
-  //     });
-  //   });
-  // }
-
-  // Future<void> fetchData() async {
-  //   const String url =
-  //       'https://staging.lotusarchi.com/search/GSLAJ'; // Contoh API
-  //   try {
-  //     final response = await http.get(Uri.parse(url));
-  //     if (response.statusCode == 200) {
-  //       final data = jsonDecode(response.body);
-  //       print('Data dari API: $data'); // Cetak ke console
-  //     } else {
-  //       print('Gagal mengambil data. Status code: ${response.statusCode}');
-  //     }
-  //   } catch (e) {
-  //     print('Error: $e');
-  //   }
-  // }
 
   Future<List<Map<String, dynamic>>?> fetchData(String searchQuery) async {
     final String url =
